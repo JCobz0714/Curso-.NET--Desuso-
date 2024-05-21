1c7ced2f7dd7e414b2abcdb69cc151f57eeacea8
J. cob <55332149+JacoboOsorio@users.noreply.github.com>
2024-05-21 04:05:46 +0000

    Lectura 34 - Listas y metodos

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+// Declaracion basica de un array
+string[] personas = { "Juan", "Maria", "Pedro", "Ana"};
+
+// Declaracion List Collection
+// Dentro de la notacion diamante, se ingresa el tipo de dato
+// que la lista va a contener
+List<string> stringList = new List<string>();
+
+// Agregar data a la List desde un array string[]
+stringList.AddRange(personas);
+
+// Agregar un solo elemento a la List
+stringList.Add("Luis");
+
+// Cuantos elementos tiene una lista (.Count)
+Console.WriteLine($"La lista stringList tiene {stringList.Count} elementos");
+
+// Metodo para imprimir todos los elementos de la lista
+ImprimirLista(stringList);
+
+void ImprimirLista(List<string> listaParaImprimir){
+  // Bucle foreach para iterar sobre los elementos de la lista
+  foreach(string elemento in listaParaImprimir) Console.WriteLine(elemento);
+}
