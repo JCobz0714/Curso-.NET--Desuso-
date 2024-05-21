1d9afaa2693399e7782a4a9f6ac48786cfffbecf
J. cob <55332149+JacoboOsorio@users.noreply.github.com>
2024-05-21 22:56:43 +0000

    Lectura 35 - Operadores de List

--- Program.cs
+++ Program.cs
@@ -18,7 +18,29 @@ Console.WriteLine($"La lista stringList tiene {stringList.Count} elementos");
 // Metodo para imprimir todos los elementos de la lista
 ImprimirLista(stringList);
 
+// Eliminar un elemento de la lista
+// Entre parentesis va la posicion del elemento que se desea eliminar
+stringList.RemoveAt(3);
+
+// Si quisiera eliminar un valor en especifico
+stringList.Remove("Maria");
+
+// Organizar los elementos de una lista (en este caso en orden alfabetico)
+stringList.Sort();
+  
 void ImprimirLista(List<string> listaParaImprimir){
   // Bucle foreach para iterar sobre los elementos de la lista
   foreach(string elemento in listaParaImprimir) Console.WriteLine(elemento);
 }
+
+//Retorna true o false si el elemento existe en la lista
+stringList.Contains("Javier");
+
+//Buscar elementos en la lista que cumplan con la condicion dada
+// la x de la expresion lambda representa el valor que va a tomar de la lista para
+// la comparacion de la condicion
+stringList.Exists(x => x.Length > 10);
+
+//Buscar un elemento en la lista que contenga un caracter en especifico
+//en este caso, que empiece con la letra "v"
+stringList.Find(x => x.StartsWith("v"));
